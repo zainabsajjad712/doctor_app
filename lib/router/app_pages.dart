53137997f0c4d2675046b38d26997765dbb9ed3a
zainabsajjad712 <zainabsajjad712@gmail.com>
2026-02-25 11:33:53 +0500

    Initial commitagain

--- lib/router/app_pages.dart
+++ lib/router/app_pages.dart
@@ -1,3 +1,7 @@
+import 'package:doctor_app/src/features/auth/otp/otp_binding.dart';
+import 'package:doctor_app/src/features/auth/otp/otp_view.dart';
+import 'package:doctor_app/src/features/auth/phone_verification/phone_binding.dart';
+import 'package:doctor_app/src/features/auth/phone_verification/phone_view.dart';
 import 'package:doctor_app/src/features/onboarding/onboarding_binding.dart';
 import 'package:doctor_app/src/features/onboarding/onboarding_view.dart';
 import 'package:get/get.dart';
@@ -10,6 +14,16 @@ class AppPages {
       name: AppRoutes.onboarding,
       page: () => const OnboardingView(),
       binding: OnboardingBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.phone,
+      page: () => const PhoneView(),
+      binding: PhoneBinding(),
+    ),
+    GetPage(
+      name: AppRoutes.otp,
+      page: () => const OtpView(),
+      binding: OtpBinding(),
     ),
   ];
 }
